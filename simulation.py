5d7fcd069607d10dce4f62cc012f6620e5a3831e
VEXI19 <87818072+VEXI19@users.noreply.github.com>
2024-12-04 22:21:07 +0100

    update default entry files

--- simulation.py
+++ simulation.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 rocket = Rocket(1)
-environment = Environment(1, 1, 1, 1, [1, 1, 1], [1, 1, 1])
+environment = Environment(1, 1, 20, 1, [1, 1, 1], [1, 1, 1])
 physics_engine = PhysicsEngine(rocket, environment, 0.1, file_path="./Simulations/Rocket_2")
 
 physics_engine.add_force(Forces.gravity)
