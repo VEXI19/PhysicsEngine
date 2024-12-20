169e31d5dbec76abf106614ad88bee2b5994b01b
VEXI19 <87818072+VEXI19@users.noreply.github.com>
2024-12-20 19:27:20 +0100

    added doc comments to Config.py and IEnvironment.py

--- PhysicsEngine/Config/Config.py
+++ PhysicsEngine/Config/Config.py
@@ -2,6 +2,12 @@ from configparser import ConfigParser
 import os
 
 class Config:
+    """
+    Class for handling config (creating default, reading and writing)
+
+    Config file (config.ini) will be created on first use of this class in the root directory of the project.
+    """
+
     def __init__(self):
         self.base_dir = os.path.dirname(os.path.realpath(__file__))
         self.config_path = os.path.abspath(os.path.join(__file__, "..", "..", "..", "config.ini"))
@@ -10,11 +16,28 @@ class Config:
             self.restore_to_default()
 
     def _read(self):
+        """
+        Reads the config file
+
+        Returns:
+            ConfigParser: ConfigParser object
+        """
         config = ConfigParser()
         config.read(self.config_path)
         return config
 
-    def set_value(self, section, key, value):
+    def set_value(self, section: str, key: str, value: str) -> str:
+        """
+        Sets the value in the config file
+        Args:
+            section (string): section from the config file
+            key (string): key of the config setting
+            value (string): value to set the key in section to
+
+        Returns:
+            ConfigParser: ConfigParser object
+
+        """
         config = ConfigParser()
         config.read(self.config_path)
         config.set(section, key, value)
@@ -23,11 +46,32 @@ class Config:
 
         return config
 
-    def __getitem__(self, item):
+    def __getitem__(self, item: str) -> str:
+        """
+        Get the value of the item from the config file
+
+        Use double brackets to specify section and key
+
+        Examples:
+            config["Section"]["Key"]
+
+        Args:
+            item (string): section or key from the config file
+
+        Returns:
+            string: value of the item
+
+        """
         config = self._read()
         return config[item]
 
     def restore_to_default(self):
+        """
+        Restores config file to default
+
+        Returns:
+            None
+        """
         config = ConfigParser()
 
         config["SIMULATION"] = {
