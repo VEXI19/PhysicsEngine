193978caa079f537c6c14b2fbf0322443df5d555
VEXI19 <87818072+VEXI19@users.noreply.github.com>
2024-12-20 23:04:23 +0100

    updated doc comments of force functions

--- PhysicsEngine/Forces/Drag.py
+++ PhysicsEngine/Forces/Drag.py
@@ -2,15 +2,20 @@ from ..Object.IAerodependent import IAerodependent
 from ..Environment.IEnvironment import IEnvironment
 import numpy as np
 from ..Utils.EulerAngles import transform_to_global, transform_to_local
+from numpy.typing import NDArray
 
 
-def drag(object: IAerodependent, environment: IEnvironment, time: float):
+def drag(object: IAerodependent, environment: IEnvironment, time: float) -> (NDArray[np.float64], NDArray[np.float64]):
     """
     Function to compute drag force and torque
-    @param object: object of the rocket with implemented IAerodependent interface
-    @param environment: object of the environment with implemented IEnvironment interface
-    @param time: current time of the simulation
-    @return: drag force and torque
+
+    Args:
+        object (IAerodependent): object on which drag is acting
+        environment (IEnvironment): simulation environment
+        time (float): current time of the simulation
+
+    Returns:
+        (NDArray[np.float64], NDArray[np.float64]): drag force and torque
     """
     if (object.velocity == np.array([0, 0, 0], dtype=float)).all():
         return np.array([0, 0, 0], dtype=float), np.array([0, 0, 0], dtype=float)
--- PhysicsEngine/Forces/Gravity.py
+++ PhysicsEngine/Forces/Gravity.py
@@ -1,12 +1,26 @@
 from ..Object.IObject import IObject
 from ..Environment.IEnvironment import IEnvironment
 import numpy as np
+from numpy.typing import NDArray
 from ..Utils import Constants
 from ..Utils.EulerAngles import transform_to_local
 
 
-def gravity(object: IObject, environment: IEnvironment, time: float):
+def gravity(object: IObject, environment: IEnvironment, time: float) -> (NDArray[np.float64], NDArray[np.float64]):
+    """
+    Function to compute gravity force and torque
+
+    Args:
+        object (IObject): object on which gravity is acting
+        environment (IEnvironment): simulation environment
+        time (float): current time of the simulation
+
+    Returns:
+        (NDArray[np.float64], NDArray[np.float64]): gravity force and torque
+    """
+
     global_gravity = np.array([0, 0, -object.mass * Constants.GRAVITATIONAL_ACCELERATION.value], dtype=float)
     local_gravity = transform_to_local(global_gravity, object.rotation)
     torque = np.array([0, 0, 0], dtype=float)
+
     return local_gravity, torque
