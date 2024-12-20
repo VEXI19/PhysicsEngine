1e3fa56751e1de8b60c5a120db079cfe3e0a9766
VEXI19 <87818072+VEXI19@users.noreply.github.com>
2024-12-21 00:28:35 +0100

    deleted ICube interface

--- PhysicsEngine/Object/__init__.py
+++ PhysicsEngine/Object/__init__.py
@@ -1,6 +1,5 @@
 from .IAerodependent import IAerodependent
 from .IObject import IObject
 from .IThrust import IThrust
-from .ICube import ICube
 
-__all__ = ['IAerodependent', 'IObject', 'IThrust', 'ICube']
+__all__ = ['IAerodependent', 'IObject', 'IThrust']
